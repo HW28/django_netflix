ccab329dbd109542abfa6a2711ef01554e0788a0
Hao Wu <wu@MacBook-Air-de-Hao.local>
2023-08-13 22:05:47 +0200

    Add styles, register, login and logout

--- django_netflix_clone/urls.py
+++ django_netflix_clone/urls.py
@@ -18,9 +18,17 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static 
+from netflix.views import index_view 
+from netflix.views import login_view 
+from netflix.views import register_view 
+from netflix.views import logout_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', index_view, name='home'),
+    path('register', register_view, name='register'), 
+    path('login', login_view, name='login'),
+    path('logout', logout_view, name='logout'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
